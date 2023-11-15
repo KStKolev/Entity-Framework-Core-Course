8a3d671b6d71387c76920231caaf08fa351c0c8f
Kolio Kolev <114016354+KStKolev@users.noreply.github.com>
2023-11-15 23:55:46 +0200

    Add files via upload

---
+++ E04. Advanced Querying/P02. Age_Restriction/BookShop/StartUp.cs
@@ -0,0 +1,38 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System.Collections;
+    using System.Data.Common;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            /*DbInitializer.ResetDatabase(db);*/
+            Console.WriteLine(GetBooksByAgeRestriction(db, "teEN"));
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command) 
+        {
+            if (!Enum.TryParse<AgeRestriction>(command, true, out var result)) 
+            {
+                Console.WriteLine($"{command} is not AgeRestricted.");
+            }
+
+            var books = context.Books.Where(b => b.AgeRestriction == result)
+                    .Select(r => new
+                    {
+                        r.Title
+                    })
+                    .OrderBy(r => r.Title)
+                    .ToList();
+
+            return string.Join(Environment.NewLine, books.Select(b => b.Title));
+        }
+    }
+}
+
+
