1992882dca05ce7202b3044f54a0b086a1bf2e4c
Kolio Kolev <114016354+KStKolev@users.noreply.github.com>
2023-12-02 23:59:42 +0200

    Add files via upload

---
+++ E05. JavaScript Object Notation - JSON/P09. Import Suppliers/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using CarDealer.Data;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson) 
+        {
+            var suppliers = JsonConvert.DeserializeObject<Supplier[]>(inputJson);
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+            return $"Successfully imported {suppliers.Count()}.";
+        }
+    }
+}
