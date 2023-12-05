cd3cafa64609788c1316b72eb24dbbee651a400c
Kolio Kolev <114016354+KStKolev@users.noreply.github.com>
2023-12-05 12:29:47 +0200

    Add files via upload

---
+++ E05. JavaScript Object Notation - JSON/P11. Import Cars/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using CarDealer.Data;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson) 
+        {
+            var suppliers = JsonConvert.DeserializeObject<Supplier[]>(inputJson);
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+            return $"Successfully imported {suppliers.Count()}.";
+        }
+
+        public static string ImportParts(CarDealerContext context, string inputJson) 
+        {
+            var parts = JsonConvert.DeserializeObject<Part[]>(inputJson);
+            var suppliedParts = parts
+                .Where(p => p.SupplierId != null)
+                .ToArray();
+            context.Parts.AddRange(suppliedParts);
+            context.SaveChanges();
+            return $"Successfully imported {suppliedParts.Count()}.";
+        }
+
+        public static string ImportCars(CarDealerContext context, string inputJson) 
+        {
+            var cars = JsonConvert.DeserializeObject<Car[]>(inputJson);
+            context.Cars.AddRange(cars);
+            context.SaveChanges();
+            return $"Successfully imported {cars.Count()}."; ;
+        }
+    }
+}
