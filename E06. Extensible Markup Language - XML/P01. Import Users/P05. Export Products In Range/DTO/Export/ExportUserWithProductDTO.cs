1951a06bf5ac48e3e956a7d50e50bacc1e78e5c2
Kolio Kolev <114016354+KStKolev@users.noreply.github.com>
2023-12-07 12:33:10 +0200

    Add files via upload

---
+++ E06. Extensible Markup Language - XML/P01. Import Users/P05. Export Products In Range/DTO/Export/ExportUserWithProductDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ProductShop.ProductShop.DTO.Export
+{
+    [XmlType("User")]
+    public class ExportUserWithProductDTO
+    {
+        [XmlElement("firstName")]
+        public string FirstName { get; set; }
+
+        [XmlElement("lastName")]
+        public string LastName { get; set; }
+
+        [XmlArray("soldProducts")]
+        public virtual ExportBoughtProductDTO[] ProductsSold { get; set; }
+    }
+}
---
+++ E06. Extensible Markup Language - XML/P01. Import Users/P05. Export Products In Range/ProductShopProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ProductShop.Models;
+using ProductShop.ProductShop.DTO.Export;
+using ProductShop.ProductShop.DTO.Import;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            CreateMap<ImportUserDTO, User>();
+            CreateMap<ImportProductDTO, Product>();
+            CreateMap<ImportCategoryDTO, Category>();
+            CreateMap<ImportCategoryProductDTO, CategoryProduct>();
+        }
+    }
+}
