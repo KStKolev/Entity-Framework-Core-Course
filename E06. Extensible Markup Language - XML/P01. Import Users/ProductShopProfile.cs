20f1f6b65546daf9a8060eca297232c51ced7562
Kolio Kolev <114016354+KStKolev@users.noreply.github.com>
2023-12-07 12:27:01 +0200

    Add files via upload

---
+++ E06. Extensible Markup Language - XML/P01. Import Users/ProductShopProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ProductShop.Models;
+using ProductShop.ProductShop.DTO.Import;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            CreateMap<ImportUserDTO, User>();
+        }
+    }
+}
---
+++ E06. Extensible Markup Language - XML/P01. Import Users/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using ProductShop.Data;
+using ProductShop.Models;
+using ProductShop.ProductShop.DTO.Import;
+using System.Xml.Serialization;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+            string filePath = "D:\\Programming\\Entity_Framework\\XML_Processing_Exercise\\ProductShop\\ProductShop\\Datasets\\users.xml";
+            var xmlReadFile = File.ReadAllText(filePath);
+            Console.WriteLine(ImportUsers(context, xmlReadFile));
+        }
+
+        public static Mapper GetMapper() 
+        {
+            var configuration = new MapperConfiguration(c => c.AddProfile<ProductShopProfile>());
+            return new Mapper(configuration);
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputXml) 
+        {
+            var xmlSerializer = new XmlSerializer(typeof(ImportUserDTO[]), new XmlRootAttribute("Users"));
+
+            using StringReader stringReader = new StringReader(inputXml);
+            ImportUserDTO[] importUsers = (ImportUserDTO[]) xmlSerializer.Deserialize(stringReader);
+
+            var mapper = GetMapper();
+            User[] users = mapper.Map<User[]>(importUsers);
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Count()}";
+        }
+    }
+}
