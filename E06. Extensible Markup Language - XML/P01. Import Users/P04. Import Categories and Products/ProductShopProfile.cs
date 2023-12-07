41333ea2fb5fbecb1a74f13f4332ca0c2fcd6de0
Kolio Kolev <114016354+KStKolev@users.noreply.github.com>
2023-12-07 12:31:31 +0200

    Add files via upload

---
+++ E06. Extensible Markup Language - XML/P01. Import Users/P04. Import Categories and Products/ProductShopProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ProductShop.Models;
+using ProductShop.ProductShop.DTO.Import;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            CreateMap<ImportUserDTO, User>();
+            CreateMap<ImportProductDTO, Product>();
+            CreateMap<ImportCategoryDTO, Category>();
+            CreateMap<ImportCategoryProductDTO, CategoryProduct>();
+        }
+    }
+}
