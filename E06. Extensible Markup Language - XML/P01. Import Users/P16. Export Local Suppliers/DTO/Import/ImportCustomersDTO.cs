174e5a02657c728195cf631e79d0efd5aafb7d25
Kolio Kolev <114016354+KStKolev@users.noreply.github.com>
2023-12-08 17:50:19 +0200

    Add files via upload

---
+++ E06. Extensible Markup Language - XML/P01. Import Users/P16. Export Local Suppliers/DTO/Import/ImportCustomersDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CarDealer.CarDealer.DTO.Import
+{
+    [XmlType("Customer")]
+    public class ImportCustomersDTO
+    {
+        [XmlElement("name")]
+        public string Name { get; set; } = null!;
+
+        [XmlElement("birthDate")]
+        public DateTime BirthDay { get; set; }
+
+        [XmlElement("isYoungDriver")]
+        public bool IsYoungDriver { get; set; }
+    }
+}
