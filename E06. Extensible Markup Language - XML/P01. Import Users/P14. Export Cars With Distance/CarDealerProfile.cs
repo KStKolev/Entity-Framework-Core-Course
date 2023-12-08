ec930978b2e5d57a98caad12dfdbdeec6160eda1
Kolio Kolev <114016354+KStKolev@users.noreply.github.com>
2023-12-08 17:49:12 +0200

    Add files via upload

---
+++ E06. Extensible Markup Language - XML/P01. Import Users/P14. Export Cars With Distance/CarDealerProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using CarDealer.CarDealer.DTO.Export;
+using CarDealer.CarDealer.DTO.Import;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            //Import
+            CreateMap<ImportSuppliersDTO, Supplier>();
+            CreateMap<ImportPartsDTO, Part>();
+            CreateMap<ImportPartToCar, ImportCarsDTO>();
+            CreateMap<ImportCarsDTO, Car>();
+            CreateMap<ImportCustomersDTO, Customer>();
+            CreateMap<ImportSalesDTO, Sale>();
+
+            //Export
+            CreateMap<Car, ExportCarsWithDistance>();
+        }
+    }
+}
