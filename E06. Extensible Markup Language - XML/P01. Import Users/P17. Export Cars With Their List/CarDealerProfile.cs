d7c319c27914935e584e747380ce2430c303a070
Kolio Kolev <114016354+KStKolev@users.noreply.github.com>
2023-12-08 17:50:48 +0200

    Add files via upload

---
+++ E06. Extensible Markup Language - XML/P01. Import Users/P17. Export Cars With Their List/CarDealerProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using CarDealer.CarDealer.DTO.Export;
+using CarDealer.CarDealer.DTO.Export.SubExportClasses;
+using CarDealer.CarDealer.DTO.Import;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            //Import
+            CreateMap<ImportSuppliersDTO, Supplier>();
+            CreateMap<ImportPartsDTO, Part>();
+            CreateMap<ImportPartToCar, ImportCarsDTO>();
+            CreateMap<ImportCarsDTO, Car>();
+            CreateMap<ImportCustomersDTO, Customer>();
+            CreateMap<ImportSalesDTO, Sale>();
+
+            //Export
+            CreateMap<Car, ExportCarsWithDistance>();
+            CreateMap<Car, ExportCarsFromMakeBMWDTO>();
+            CreateMap<Supplier, ExportLocalSuppliersDTO>();
+            CreateMap<Car, ExportCarsWithTheirListOfPartsDTO>();
+            CreateMap<ExportListOfPartsDTO, ExportCarsWithTheirListOfPartsDTO>();
+            CreateMap<ListOfParts, ExportListOfPartsDTO>();
+        }
+    }
+}
